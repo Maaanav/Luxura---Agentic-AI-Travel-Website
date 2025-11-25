5e6168fbcfb8d3e128cbab966fee467c90427ce5
Maaanav <113304883+Maaanav@users.noreply.github.com>
2025-11-25 15:52:59 +0530

    Create Home component with Hero, Info, InputForm, and Footer

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Hero from "../components/Hero";
+import Info from "../components/Info";
+import InputForm from "../components/InputForm";
+import Footer from "../components/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Info />
+      <InputForm />
+      <Footer />
+    </>
+  );
+}
